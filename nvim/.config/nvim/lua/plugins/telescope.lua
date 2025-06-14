085bfc4e6f2f6d03cf9e1335e9d30f60848dd9a2
Sergio Escobar <sergio@Sergios-MacBook-Pro.local>
2025-06-14 12:28:03 -0500

    Added plugings for vim

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,50 +1,50 @@
 return {
   "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
+  tag = '0.1.8',
   dependencies = {
     "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "nvim-tree/nvim-web-devicons",
-    "folke/todo-comments.nvim",
   },
   config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
-    local transform_mod = require("telescope.actions.mt").transform_mod
+    local actions = require('telescope.actions')
+      require('telescope').setup({
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-k>"] = actions.move_selection_previous,                       -- move to prev result
+              ["<C-j>"] = actions.move_selection_next,                           -- move to next result
+              ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
+            }
+          }
+        }
+      })
 
-    local trouble = require("trouble")
-    local trouble_telescope = require("trouble.sources.telescope")
-
-    -- or create your custom action
-    local custom_actions = transform_mod({
-      open_trouble_qflist = function(prompt_bufnr)
-        trouble.toggle("quickfix")
-      end,
-    })
-
-    telescope.setup({
-      defaults = {
-        path_display = { "smart" },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-            ["<C-t>"] = trouble_telescope.open,
-          },
-        },
-      },
-    })
-
-    telescope.load_extension("fzf")
-
-    -- set keymaps
     local keymap = vim.keymap -- for conciseness
+    local builtin = require("telescope.builtin")
+    keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Fuzzy find recent files" })
+    keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find string in cwd" })
+    keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find string under cursor in cwd" })
 
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-  end,
+    -- Rip grep + Fzf
+    vim.keymap.set('n', '<leader>fg', function()
+      builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    end)
+
+    -- Find instance instance of current view being included
+    vim.keymap.set('n', '<leader>fc', function()
+      local filename_without_extension = vim.fn.expand('%:t:r')
+      builtin.grep_string({ search = filename_without_extension })
+    end, { desc = "Find current file: " })
+
+    -- Grep current string (for when gd doesn't work)
+    vim.keymap.set('n', '<leader>fs', function()
+      builtin.grep_string({})
+    end, { desc = "Find current string: " })
+
+    -- find files in vim config
+    vim.keymap.set('n', '<leader>fi', function()
+      builtin.find_files({ cwd = "~/.config/nvim/" });
+    end)
+
+  end
 }
